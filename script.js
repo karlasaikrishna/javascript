286cc6cdbbf483847f212e33f657d1b2bb359332
ROLEX <karlasaikrishna1122.com>
2025-10-17 14:05:48 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,25 @@
+a=76
+console.log(a)
+stname ='hello'
+console.log(typeof(stname))
+var departement = null
+console.log(typeof(departement))
+var studentdata = [23,78,9,4,230]
+console.log(studentdata)
+console.log(studentdata.length)
+console.log(studentdata[0]=10)
+console.log(studentdata.slice(0,2))
+const emp_data = {
+    emp_id : 56,
+    emp_name :  "saikrishna",
+    emp_add : "hyderabad"
+}
+const emp_data2 = {
+    emp_id : 25,
+    emp_name :  "mani",
+    emp_add : "hyderabad"
+}
+ var list1=[emp_data,emp_data2]
+console.log(list1)
+
+console.log(list1[0].emp_name)
